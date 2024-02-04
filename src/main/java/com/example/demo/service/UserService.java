ce784214bd780378a32a0deab5988099e9fc96b3
Данил Дунаев <danil.dunik@gmail.com>
2024-02-04 16:01:53 +0300

    добавил set get

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,10 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.dao.UserDao;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Setter
+@Getter
 public class UserService {
      private UserDao userDao;
 
