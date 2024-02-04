09c6ec590578f1a9935350596ae682750e6d0ef6
Данил Дунаев <danil.dunik@gmail.com>
2024-02-04 15:58:29 +0300

    добавил дао слой

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.UserDao;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService {
+     private UserDao userDao;
 
 }
