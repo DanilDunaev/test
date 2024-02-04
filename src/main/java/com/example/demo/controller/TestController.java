43953cb2bd9ced7fee07c50aa9e01555686b0431
Данил Дунаев <danil.dunik@gmail.com>
2024-02-04 15:55:42 +0300

    добавил сервис

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class TestController {
-
+    private UserService userService;
 
 }
